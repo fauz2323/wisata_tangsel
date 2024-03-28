29bc628799f1770fc030918f1f4ee6da87a509d1
Zen <59063107+fauz2323@users.noreply.github.com>
2024-03-28 13:12:21 +0700

    add

---
+++ android/app/src/main/kotlin/com/example/joki_wisata_tangsel/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.joki_wisata_tangsel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/tha_maps/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tha_maps
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
